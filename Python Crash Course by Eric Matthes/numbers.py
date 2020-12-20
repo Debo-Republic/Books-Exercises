e910e73bdd9c87b108171f4c3087f1b5c2a57406
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-20 18:06:15 -0500

    Add files via upload
    added the last part of variables and numbers

---
+++ Python Crash Course by Eric Matthes/numbers.py
@@ -0,0 +1,18 @@
+#The use of underscore 
+a_long_ass_number = 1_000_000_000_000
+my_net_worth_by_30 = 2_000_000_000
+t5, t6, t7, t8, t9, t30 = 100000, 150000,200000,250000,300000, 400000
+#denoting constants 
+ALL_TIME_HIGH = 50_000_000
+print(a_long_ass_number, t5, t6, t8, t9)
+#print 8 by operations 
+print(5+3)
+print(2**3)
+print(10-2)
+print(4*2)
+
+#2.9
+FAV_NUM = 5251999
+message = f"My favourite number these days is {FAV_NUM}"
+print(message)
+import this
