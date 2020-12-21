b8514e51924379109109d90f905b4c90a31cf9a6
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-21 18:27:31 -0500

    Add files via upload
    Added conditional statements and testing conditional statement

---
+++ Python Crash Course by Eric Matthes/conditional_statements.py
@@ -0,0 +1,7 @@
+#understanding if statements 
+moisturizers = ['cosrx', 'klairs', 'sunday', 'yttp']
+for cream in moisturizers:
+	if cream == 'cosrx' :
+		print(f"the only good cream is {cream}")
+	else :
+		print(f"{cream} sucks")
