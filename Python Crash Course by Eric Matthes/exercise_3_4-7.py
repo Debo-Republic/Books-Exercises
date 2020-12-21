afc1935bc45e901d5fac655e1f5c01029371a972
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-20 23:18:23 -0500

    Add files via upload
    added an exercise with lists

---
+++ Python Crash Course by Eric Matthes/exercise_3_4-7.py
@@ -0,0 +1,21 @@
+guest_list = ["Shantopriyo Bhowmick", "Jordan B Peterson", "Mayuri B Upadhaya", "Deblina Bhowmick", "Sandeep Goswami", "Soumen Goswami"]
+print(f"Hi my dear brother{guest_list[0].title()}, Hope your killing it wherever you are. Come join me for a dinner. Meet people i like the most")
+print(f"Hi professor {guest_list[1].title()} why don't you bless us with your divine presence & see for once who all your words get to affect")
+print(f"My baby {guest_list[2]}, meet the most important folks in my life")
+print(f"Hi mom {guest_list[3]}, bless everyone on the table with your grace & nature.")
+print(f" My best friends{guest_list[-1], guest_list[-2]}, see the world laid out in a table")
+
+
+print(f" My friend {guest_list.pop()} can't make it beacause he is not done styling his hair.")
+guest_list.append("Shrinjit Dash")
+
+guest_list.pop(1)
+print(guest_list)
+guest_list.pop(1)
+guest_list.pop()
+guest_list.pop()
+print(guest_list)
+del guest_list[0]
+del guest_list[0]
+print(guest_list)
+
