651968f88acad128789ce2641f13a5364624eb93
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-21 01:18:13 -0500

    Add files via upload
    added code about working more with lists

---
+++ Python Crash Course by Eric Matthes/magicians.py
@@ -0,0 +1,17 @@
+magicians = {"dalton", "Bordon", "charles"}
+for magician in magicians:
+	print(f"{magician.title()}, Bugger that was a great trick!" )
+
+print("Big THANKYOU for being here")
+
+pizzas = ['margherita', 'pepperoni', 'buffalo']
+for pizza in pizzas:
+	print(f"I love {pizza} more than burgers")
+
+print("Basically I love pizzas that New York has to offer!")
+
+animals = ['dolphins', 'lobsters', 'apes']
+for animal in animals:
+	print(f"Would you like to pet {animal}")
+
+print("All the animals make great pets ")
