2d60f499e98e300e515f818b9daca821e7e9005b
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-21 21:12:01 -0500

    Add files via upload
    understanding tests and assignment.

--- Python Crash Course by Eric Matthes/conditional_statements.py
+++ Python Crash Course by Eric Matthes/conditional_statements.py
@@ -5,3 +5,18 @@ for cream in moisturizers:
 		print(f"the only good cream is {cream}")
 	else :
 		print(f"{cream} sucks")
+
+#Conditional tests on equality 
+finals = ['mi', 'ams', 'ml']
+today = 'ams'
+
+if today == 'ams':
+	print(f"today was {today} finals. Probably going to ace it.")
+
+#Conditional test on checking for inequality 
+if today != 'ams':
+	print("not equal to anything.")
+else:
+	print(f"How did your {today} paper go ?")
+
+# and, or and membership functions 
