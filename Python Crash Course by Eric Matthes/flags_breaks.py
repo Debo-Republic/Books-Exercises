94e691a91c67eb78757b4b58cdda16676d14a998
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-24 15:33:28 -0500

    Add files via upload
    added programs on loop exit conditions

---
+++ Python Crash Course by Eric Matthes/flags_breaks.py
@@ -0,0 +1,9 @@
+#using flags to control the flow of the program
+flag = True
+while(flag):
+    mess = input("I'll repeat everything you say except quit. say something")
+    if mess,lower() == 'quit':
+        flag = False
+    print(mess)
+
+#pizza toppings
