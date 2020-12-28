2feb13e46d3bf4e27f458e35a0bb4bed47969aa9
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2020-12-27 22:51:12 -0500

    Add files via upload
    handling multiple file not found exceptions

--- Python Crash Course by Eric Matthes/filenotfound_exceptions.py
+++ Python Crash Course by Eric Matthes/filenotfound_exceptions.py
@@ -1,12 +1,17 @@
-# Handling file not found exception :
-file_name = 'alice.txt'
-try:
-    with open(file_name, encoding='utf-8') as f:
-        contents = f.read()
-except FileNotFoundError:
-    print("File doesn't exist in the directory!"
-          " Please check and try again")
-else:
-    words = len(contents.split())
-    print(f"The approximate number of words in the text is {words}")
+# Handling exceptions while working with multiple files.
+def count(filename):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print("File doesn't exist in the directory!"
+              " Please check and try again")
+    else:
+        words = len(contents.split())
+        print(f"The approximate number of words in the {filename} is {words}")
+
+
+file_names = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for file_name in file_names:
+    count(file_name)
 
